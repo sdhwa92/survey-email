5a120fc4cc0b27ad126f4dcfc9dc25710fcbdb40
Daehwa Seo <sdhwa92@gmail.com>
2022-02-17 16:02:29 +1000

    test deployment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express"); // common js module
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there test" });
+  res.send({ hi: "there" });
 });
 
 const PORT = process.env.PORT || 5000;
