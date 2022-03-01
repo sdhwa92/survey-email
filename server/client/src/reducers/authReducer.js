a7364fd9144bb67bb859b08deb79187d84f27fb9
Daehwa Seo <sdhwa92@gmail.com>
2022-03-02 00:09:11 +1000

    Create front-end part, and make an initial api call

---
+++ server/client/src/reducers/authReducer.js
@@ -0,0 +1,9 @@
+const authReducer = (state = {}, action) => {
+  console.log(action);
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
---
+++ server/client/src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    ["/api", "/auth/google", "/auth/facebook"],
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+    })
+  );
+};
