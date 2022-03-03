c0fc1f69d8fa1047f413b859db7a7b30fe939f68
Daehwa Seo <sdhwa92@gmail.com>
2022-03-03 23:05:07 +1000

    Login and Logout action, and redirect users to the appropriate page.

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
-const authReducer = (state = {}, action) => {
-  console.log(action);
+import { FETCH_USER } from "../actions/types";
+
+const authReducer = (state = null, action) => {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
