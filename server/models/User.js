955608f9fd0afc1ee4129c324325933d1dcc0c53
Daehwa Seo <sdhwa92@gmail.com>
2022-03-08 00:00:34 +1000

    implement stripe charge request, and requires authentication for the API call by using a middleware

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   googleId: String,
   facebookId: String,
+  credits: { type: Number, default: 0 },
 });
 userSchema.plugin(findOrCreate);
 
